71528e294d174d3ea75f8b5ea4c4d68962ba88b2
Talha Waheed <62260739+Talha0077@users.noreply.github.com>
2021-02-03 07:16:12 +0500

    Add files via upload

---
+++ Flight Network System/cmake-build-debug/CMakeFiles/Flight_Network_System.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Flight_Network_System.dir/main.cpp.obj"
+  "Flight_Network_System.exe"
+  "Flight_Network_System.exe.manifest"
+  "Flight_Network_System.lib"
+  "Flight_Network_System.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Flight_Network_System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
